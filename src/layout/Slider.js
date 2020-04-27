8584247ecb87cd4e1ce94f67241c8999983269f1
Muhammad Awais <awaisshaikh94@gmail.com>
2020-04-28 00:45:55 +0500

    slider section done

--- src/layout/Slider.js
+++ src/layout/Slider.js
@@ -5,7 +5,23 @@ import '../assets/css/slider.css';
 function Slider() {
     return(
         <div>
-            <p>Slider s</p>
+           
+           <div className="slider">
+                <div className="container">
+                    <div className="row justify-content-center ">
+                        
+                        <div className="col-md-6">
+                            <h1>Flat 30% Off And Free Delivery</h1>
+
+                            <div className="button">
+                                <a href="#">ODER NOW</a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+           </div>
+
         </div>
     );
 }
