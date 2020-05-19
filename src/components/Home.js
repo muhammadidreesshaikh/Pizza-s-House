24bfcdbd68abedc165049a38b76cfd405681c443
Muhammad Awais <awaisshaikh94@gmail.com>
2020-05-20 02:12:34 +0500

    home section half done

--- src/components/Home.js
+++ src/components/Home.js
@@ -34,15 +34,48 @@ class Home extends React.Component{
         return(
             <div>
                 <Slider />
+
                 <div className="home">
                    <div className="container">
-                       <div className="row">
+                       <div className="row justify-content-center">
     
-                            <div className="col-sm-12 col-md-12 col-lg-12">
-                                <h3>Home</h3>
-                                <div className="seprator"></div>
+                            <div className="col-sm-4 col-md-4 col-lg-4">
+                                <div className="main">
+
+                                    <div className="buttons">
+                                        <a href='#'>Delivery</a> <a href='#'>Take Away</a>
+                                    </div>
+
+                                    <select>
+                                        <option value="volvo">Select City</option>
+                                        <option value="saab">Karachi</option>
+                                        <option value="opel">Lahore </option>
+                                        <option value="audi">Faisalabad</option>
+                                        <option value="saab">Rawalpindi</option>
+                                        <option value="opel">Hyderabad </option>
+                                        <option value="audi">Quetta</option>
+                                        <option value="audi">Peshawar</option>
+                                        <option value="audi">Gujranwala</option> 
+                                    </select>
+
+                                    <select>
+                                        <option value="volvo">Select Area</option>
+                                        <option value="saab">Liaquatabad</option>
+                                        <option value="opel">Nazimabad </option>
+                                        <option value="audi">Gulzar-e-Hijri</option>
+                                        <option value="saab">Gulshan-e-Iqbal</option>
+                                        <option value="opel">Jamshed Quarters </option>
+                                        <option value="audi">Lyari</option>
+                                        <option value="audi">Mango Pir</option>
+                                        <option value="audi">Shah Faisal</option> 
+                                    </select>
+
+                                    <div className='btn1'>
+                                        <a href="#">Oder Now</a>
+                                    </div>
+                                </div>
                             </div>
-    
+     
                        </div>
                    </div>
                 </div>
